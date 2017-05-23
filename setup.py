a73d901d57495dc0f9673699d277b55be371b362
Rik <gitaarik@gmail.com>
2017-05-23 12:19:28 +0200

    made ready for pip distribution

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='django-admin-relation-links',
+    version='0.0.1',
+    author='gitaarik',
+    author_email='gitaarik@gmail.com',
+    packages=['django_admin_relation_links'],
+    url='https://github.com/gitaarik/django-admin-relation-links/',
+    license='LGPL licence, see LICENCE.txt',
+    description='An easy way to add links to relations in the Django Admin site.',
+    long_description=open('README.md').read(),
+    zip_safe=False
+)
