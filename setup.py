f7c06982e107de29f77840202e5b2a9ca84d045c
Rik <gitaarik@posteo.net>
2019-09-26 13:17:35 +0200

    [setup.py] updated setup info

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
 from setuptools import setup
 
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='django-admin-relation-links',
     keywords='django admin relation foreignkey link',
-    version='0.2.2',
+    version='0.2.3',
     author='gitaarik',
-    author_email='gitaarik@gmail.com',
+    author_email='gitaarik@posteo.net',
     packages=['django_admin_relation_links'],
     url='https://github.com/gitaarik/django-admin-relation-links/',
-    license='LGPL licence, see LICENCE.txt',
+    license='GNU Lesser General Public License v3 (LGPLv3)',
     description='An easy way to add links to relations in the Django Admin site.',
-    zip_safe=False
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3'
 )
